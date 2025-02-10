2662337444fdbb3d589d0389094ce0ffd0da8ada
RudyJakub <kubazewsi@gmail.com>
2025-02-10 18:54:48 +0100

    read sql from file

--- cmd/tables/main.go
+++ cmd/tables/main.go
@@ -4,47 +4,21 @@ import (
 	"context"
 	"log"
 	"offgrid/internal/database"
+	"os"
 
 	_ "github.com/joho/godotenv/autoload"
 )
 
 func main() {
 	db := database.New()
-	var err error
-
-	_, err = db.Exec(context.Background(), `
-	CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`)
+	content, err := os.ReadFile("cmd/tables/tables.sql")
 
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Error reading file: %v", err)
 	}
 
-	_, err = db.Exec(context.Background(), `
-	CREATE TABLE IF NOT EXISTS users(
-		id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-		email TEXT NOT NULL UNIQUE,
-		password_hash TEXT NOT NULL,
-		created_at TIMESTAMP DEFAULT now(),
-		updated_at TIMESTAMP DEFAULT now(),
-		is_superuser BOOLEAN
-	)`)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	_, err = db.Exec(context.Background(), `
-	CREATE TABLE IF NOT EXISTS articles(
-		id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-		title TEXT UNIQUE,
-		description TEXT,
-		content TEXT,
-		created_at TIMESTAMP DEFAULT now(),
-		updated_at TIMESTAMP DEFAULT now(),
-		public BOOLEAN,
-		author_id UUID NOT NULL,
-		FOREIGN KEY (author_id) REFERENCES users(id)
-	)`)
+	sql := string(content)
+	_, err = db.Exec(context.Background(), sql)
 
 	if err != nil {
 		log.Fatal(err)
---
+++ cmd/tables/tables.sql
@@ -0,0 +1,23 @@
+	CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+	CREATE TABLE IF NOT EXISTS users(
+		id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+		email TEXT NOT NULL UNIQUE,
+		password_hash TEXT NOT NULL,
+		created_at TIMESTAMP DEFAULT now(),
+		updated_at TIMESTAMP DEFAULT now(),
+		is_superuser BOOLEAN
+	);
+
+	CREATE TABLE IF NOT EXISTS articles(
+		id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+		title TEXT UNIQUE,
+		description TEXT, 
+		content TEXT,
+		created_at TIMESTAMP DEFAULT now(),
+		updated_at TIMESTAMP DEFAULT now(),
+		public BOOLEAN,
+		author_id UUID NOT NULL,
+		FOREIGN KEY (author_id) REFERENCES users(id)
+	);
+    
